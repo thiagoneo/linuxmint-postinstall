806a4dd8d1f6ebf1d400cac40378be4c2b1abaf8
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2025-01-24 14:02:10 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -168,6 +168,7 @@ mv /usr/lib/cups/driver/driverless /usr/lib/cups/driver/disabled/
 
 # Antivirus
 \cp -rf "${SCR_DIRECTORY}"/system-files/etc/clamav/ /etc/
+\cp -rf "${SCR_DIRECTORY}"/system-files/etc/system/clamav-clamonacc.service
 chmod +x /etc/clamav/virus-event.bash
 systemctl stop clamav-freshclam
 freshclam
